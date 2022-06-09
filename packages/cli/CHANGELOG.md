1566c744a211955620e3f0f562b0cb3708d772dc
梦念幻想 <5402106+imohuan@user.noreply.gitee.com>
2022-06-09 17:56:13 +0800

    test

---
+++ packages/cli/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @imohuan/cli
+
+## 2.0.0
+
+### Major Changes
+
+- first commit
--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,2 +1,2 @@
 import chalk from "chalk";
-console.log(chalk.green.bold("Hello"));
+console.log(chalk.green.bold("Hello World"));
